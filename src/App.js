89e175c1a0476fadb83c701ca82073e2f34166cc
kyle_koh <ykoh728@gmail.com>
2018-10-03 23:43:19 +0900

    add google map thrid library

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper, Polyline, Polygon} from 'google-maps-react';
+ 
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+ 
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+    console.log(props)
+    console.log(marker)
+    console.log(e)
+  }    
 
-class App extends Component {
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
+
   render() {
+    const triangleCoords = [
+      {lat: 25.774, lng: -80.190},
+      {lat: 18.466, lng: -66.118},
+      {lat: 32.321, lng: -64.757},
+      {lat: 25.774, lng: -80.190}
+    ]
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map google={this.props.google} 
+            style={{width: '100%', height: '100%', position: 'relative'}}
+            className={'map'}
+            onClick={this.onMapClicked}
+            zoom={14}
+            initialCenter={{
+              lat: 45.519091,
+              lng: -122.679295
+            }}>
+            <Marker onClick={this.onMarkerClick}  
+                    name={'Pioneer Square'} />
+            <InfoWindow
+              content={contentString}
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>  
+            </InfoWindow>          
+      </Map>
     );
   }
 }
-
-export default App;
+ 
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyChbuMRkdicsgsk-asdQOu-qEoZajcP_P0")
+})(MapContainer)
---
+++ src/ScriptCache.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+
+
+class ScriptCache extends Component {
+  this.scriptCache = cache({
+  	google: 'https://api.google.com/some/script.js'
+	});
+
+  
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+
+
+export default ScriptCache;
