bbaaf552a17f92ef1fc9b5d6027cf2a1d2c4995f
kyle_koh <ykoh728@gmail.com>
2018-10-16 18:20:25 +0900

    add aria role for list

--- src/VenueList.js
+++ src/VenueList.js
@@ -6,15 +6,15 @@ class VenueList extends Component {
   	
     return (
 	    <ol className="venue-list-wrapper" tabindex="0">
+	    <span id="venue-label" className="venue-label">Coffee Shop List...</span>
 	      {this.props.venues && 
 	      this.props.venues.map((venue, id) =>
-	        <li key={venue.id} className="venue-list" tabindex="0" onClick={() => this.props.handleListItemClick(venue)}>
+	        <li key={venue.id} className="venue-list" tabindex="0" aria-labelledby="venue-label" onClick={() => this.props.handleListItemClick(venue)}>
 	          <img src = {`${venue.image_url ? venue.image_url : './img/cat1.jpg'}`} className="list-image" alt={`${venue.name}`} />   
 	          <span>{venue.name ? venue.name : "Unknown"}</span>
 	        </li>
 	      )}
 	    </ol>
-
     );
   }
 }
